6cd276e0ef160dd0979399803ad996de661343bc
Antonio King <antonio.j.king@gmail.com>
2021-05-20 14:58:44 -0600

    Test sequence shuffle

--- lib/secret_sequence.rb
+++ lib/secret_sequence.rb
@@ -1,2 +1,21 @@
 class SecretSequence
+  def initialize(sequence)
+    @sequence = sequence
+    @code = []
+  end
+
+  def randomize_sequence
+    initial_array = ['r', 'b', 'g', 'y']
+
+    require 'pry'; binding.pry 
+
+    4.times do
+      initial_array.shuffle
+    end
+
+
+  end
+
+
+
 end
--- spec/secret_sequence_spec.rb
+++ spec/secret_sequence_spec.rb
@@ -3,8 +3,16 @@ require './lib/secret_sequence'
 
 describe SecretSequence do
   it 'exists and has attributes' do
-    code1 = SecretSequence.new
+    code1 = SecretSequence.new('rbgy')
 
     expect(code1).to be_an(SecretSequence)
   end
+
+  it 'shuffles array' do
+    code1 = SecretSequence.new('rbgy')
+
+
+    expect(code1).not_to eq(['r', 'b', 'g', 'y'])
+  end
+
 end
