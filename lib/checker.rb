d39d2fd2b9fd954ab190a56eacd15d36a8833bcb
Jason Knoll <jason.p.knoll@gmail.com>
2021-05-22 13:58:22 -0700

    Test methods to anaylize colors

--- lib/checker.rb
+++ lib/checker.rb
@@ -13,5 +13,85 @@ class Checker
     @guess == @computer
   end
 
+  def compare_position
 
+  end
+
+
+  def r_color
+    gr = guess.count('r')
+
+  end
+
+  def colors_array  # [rbgy] - [rggy] = 4 - [g].count = 3
+    color_compare = []
+    color_compare = @guess - @computer
+  end
+
+
+  # Count R's B's G's Y's
+  def guess_colors
+    r = @guess.count('r') # = 2
+    y = @guess.count('y')
+    b = @guess.count('b')
+    g = @guess.count('g')
+    count = r + y + b + g
+  end
+
+  def computer_colors
+    r = @computer.count('r') # = 1
+    y = @computer.count('y')
+    b = @computer.count('b')
+    g = @computer.count('g')
+    count = r + y + b + g
+  end
+
+  def analyze_colors
+    gr = @guess.count('r')
+    gy = @guess.count('y')
+    gb = @guess.count('b')
+    gg = @guess.count('g')
+    gcount = gr + gy + gb + gg
+
+    cr = @computer.count('r')
+    cy = @computer.count('y')
+    cb = @computer.count('b')
+    cg = @computer.count('g')
+    ccount = cr + cy + cb + cg
+
+    return @guess.count('r') if gr - cr == 0
+  end
+  # def analyze_colors
+  #   color_counter = 0
+  #   @guess.each do |color|
+  #     4.times do
+  #       if color == @computer[0]
+  #         color_counter += 1
+  #       @computer.rotate
+  #       else
+  #       end
+  #     end
+  #   end
+  #   color_counter
+  # end
+
+
+  # returns number of correct colors (.each)
+  # def analyze_colors
+  #   color_counter = 0
+  #   @guess.each do |color|
+  #     if color == @computer[0]
+  #       color_counter += 1
+  #     elsif color == @computer[1]
+  #       color_counter += 1
+  #     elsif color == @computer[2]
+  #       color_counter += 1
+  #     elsif
+  #       color == @computer[3]
+  #         color_counter += 1
+  #     end
+  #   end
+  #   color_counter
+  #   # @guess[0] == @computer[0..3]
+  # end
 end
--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -16,15 +16,7 @@ describe Checker do
       expect(check1.guess).to eq(['r', 'b', 'g', 'y'])
     end
 
-    it "has player array" do
-    end
-
-    it "has computer array" do
-
-
-    end
-
-    it "compares" do
+    it "compares and has arrays" do
       guess1 = Guess.new('rbgy')
       game1 = Game.new(['r', 'b', 'g', 'y'])
       guess = guess1.convert_guess
@@ -33,5 +25,17 @@ describe Checker do
 
       expect(check1.compare).to eq(true)
     end
+
+    it 'Verifies colors in array' do
+      guess1 = Guess.new('rbgy')
+      game1 = Game.new(['r', 'b', 'g', 'y'])
+      guess = guess1.convert_guess
+      computer = game1.secret_sequence
+      check1 = Checker.new(guess, computer)
+
+      expect(check1.analyze_colors).to eq(5)
+    end
+
+    it 'Compares position'
   end
 end
